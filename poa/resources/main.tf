bdc5b479bf5f6e26ba53f99814ea2be3f8411c67
BerkayAygun <berkay_aygun@comcast.com>
2019-11-13 17:25:20 +0400

    Security groups added to the resource file

--- poa/resources/main.tf
+++ poa/resources/main.tf
@@ -1,3 +1,3 @@
-resource "openstack_compute_instance_v2" "pcmm_instance" {
-
+resource "openstack_compute_instance_v2" "test" {
+  security_groups = ["default","dashboard_access","inter_instance_comm","mongo_access","mw_access"]
 }
