3852844fbc0d2916be4b64ba7559d001a26c49ae
Berkay Aygun <baygun001c@megadocker-ch2-d26p.sys.comcast.net>
2019-11-26 16:01:26 +0300

    clean up

--- qa/poa/qa-test-po-a-001-port.tf
+++
@@ -1,7 +0,0 @@
-resource "openstack_networking_port_v2" "qa-test-po-a-001_port" {
-	count = "1"
-	name = "qa-test-po-a-001"
-	network_id = "5762b250-6836-4412-9221-ab2f0fb0dce5"
-	admin_state_up = "true"
-}
-
